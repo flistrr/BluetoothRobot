d509334cdbb77c296bd52f4a2920764db3c3f4fd
flistrrr <flistrrr@gmail.com>
2025-12-26 20:46:52 +0200

    init repo

---
+++ include/Robot.h
@@ -0,0 +1,186 @@
+#include <Arduino.h>
+#include <AFMotor_R4.h>
+
+class Robot
+{
+private:
+    AF_DCMotor motorFrontRight;
+    AF_DCMotor motorFrontLeft;
+    AF_DCMotor motorBackLeft;
+    AF_DCMotor motorBackRight;
+
+    int pinBuzzer;
+    int pinHeadLed;
+    int pinBackLed;
+    int pinFrontSensor;
+    int pinBackSensor;
+
+    int currentSpeed;
+
+public:
+    const int firstGear = 65;
+    const int secondGear = 130;
+    const int thirdGear = 195;
+    const int fourthGear = 255;
+
+    Robot() : motorFrontRight(1), motorFrontLeft(2), motorBackLeft(3), motorBackRight(4)
+    {
+        currentSpeed = 255;
+    }
+
+    void begin(int buzzer, int headLed, int backLed, int frontSensor, int backSensor)
+    {
+        pinBuzzer = buzzer;
+        pinHeadLed = headLed;
+        pinBackLed = backLed;
+        pinFrontSensor = frontSensor;
+        pinBackSensor = backSensor;
+
+        pinMode(pinBuzzer, OUTPUT);
+        pinMode(pinHeadLed, OUTPUT);
+        pinMode(pinBackLed, OUTPUT);
+        pinMode(pinFrontSensor, INPUT);
+        pinMode(pinBackSensor, INPUT);
+
+        stop();
+    }
+
+    void setSpeed(int targetSpeed)
+    {
+        currentSpeed = targetSpeed;
+        updateMotorSpeed(targetSpeed, targetSpeed, targetSpeed, targetSpeed);
+    }
+
+    void updateMotorSpeed(int frontLeft, int frontRight, int backLeft, int backRight)
+    {
+        motorFrontLeft.setSpeed(frontLeft);
+        motorFrontRight.setSpeed(frontRight);
+        motorBackLeft.setSpeed(backLeft);
+        motorBackRight.setSpeed(backRight);
+    }
+
+    bool checkObstacle()
+    {
+        if (digitalRead(pinFrontSensor) == LOW || digitalRead(pinBackSensor) == LOW)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void horn()
+    {
+        tone(pinBuzzer, 1000);
+        delay(50);
+        noTone(pinBuzzer);
+    }
+
+    void runMotors(uint8_t dir)
+    {
+        motorFrontLeft.run(dir);
+        motorFrontRight.run(dir);
+        motorBackLeft.run(dir);
+        motorBackRight.run(dir);
+    }
+
+    void checkIfDirectionIsBlocked()
+    {
+        if (checkObstacle())
+        {
+            stop();
+            horn();
+            return;
+        }
+    }
+
+    void forward()
+    {
+        checkIfDirectionIsBlocked();
+        updateMotorSpeed(currentSpeed, currentSpeed, currentSpeed, currentSpeed);
+        runMotors(FORWARD);
+    }
+
+    void back()
+    {
+        checkIfDirectionIsBlocked();
+        digitalWrite(pinBackLed, HIGH);
+        updateMotorSpeed(currentSpeed, currentSpeed, currentSpeed, currentSpeed);
+        runMotors(BACKWARD);
+    }
+
+    void left()
+    {
+        updateMotorSpeed(currentSpeed, currentSpeed, currentSpeed, currentSpeed);
+        motorFrontLeft.run(BACKWARD);
+        motorBackLeft.run(BACKWARD);
+        motorFrontRight.run(FORWARD);
+        motorBackRight.run(FORWARD);
+    }
+
+    void right()
+    {
+        updateMotorSpeed(currentSpeed, currentSpeed, currentSpeed, currentSpeed);
+        motorFrontLeft.run(FORWARD);
+        motorBackLeft.run(FORWARD);
+        motorFrontRight.run(BACKWARD);
+        motorBackRight.run(BACKWARD);
+    }
+
+    void forwardLeft()
+    {
+        checkIfDirectionIsBlocked();
+        int slow = currentSpeed / 4;
+        updateMotorSpeed(slow, currentSpeed, slow, currentSpeed);
+        runMotors(FORWARD);
+    }
+
+    void forwardRight()
+    {
+        checkIfDirectionIsBlocked();
+        int slow = currentSpeed / 4;
+        updateMotorSpeed(currentSpeed, slow, currentSpeed, slow);
+        runMotors(FORWARD);
+    }
+
+    void backLeft()
+    {
+        checkIfDirectionIsBlocked();
+        digitalWrite(pinBackLed, HIGH);
+        int slow = currentSpeed / 4;
+        updateMotorSpeed(slow, currentSpeed, slow, currentSpeed);
+        runMotors(BACKWARD);
+    }
+
+    void backRight()
+    {
+        checkIfDirectionIsBlocked();
+        digitalWrite(pinBackLed, HIGH);
+        int slow = currentSpeed / 4;
+        updateMotorSpeed(currentSpeed, slow, currentSpeed, slow);
+        runMotors(BACKWARD);
+    }
+
+    void stop()
+    {
+        digitalWrite(pinBackLed, LOW);
+        motorFrontLeft.run(RELEASE);
+        motorFrontRight.run(RELEASE);
+        motorBackLeft.run(RELEASE);
+        motorBackRight.run(RELEASE);
+    }
+
+    void lightsOn()
+    {
+        digitalWrite(pinHeadLed, HIGH);
+    }
+
+    void lightsOff()
+    {
+        digitalWrite(pinHeadLed, LOW);
+    }
+
+    void horn()
+    {
+        horn();
+    }
+};
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "Robot.h"
+
+Robot bluetoothRobot;
+
+const int PIN_BUZZER = 2;
+const int PIN_HEAD_LED = A5;
+const int PIN_BACK_LED = A4;
+const int PIN_FRONT_SENSOR = A1; 
+const int PIN_BACK_SENSOR = A2; 
+
+void setup() {
+  Serial.begin(9600);
+  bluetoothRobot.begin(PIN_BUZZER, PIN_HEAD_LED, PIN_BACK_LED, PIN_FRONT_SENSOR, PIN_BACK_SENSOR);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char cmd = Serial.read();
+    
+    switch (cmd) {
+      case 'F': bluetoothRobot.forward(); break;
+      case 'B': bluetoothRobot.back(); break;
+      case 'L': bluetoothRobot.left(); break;
+      case 'R': bluetoothRobot.right(); break;
+      
+      case 'G': bluetoothRobot.forwardLeft(); break;
+      case 'H': bluetoothRobot.forwardRight(); break;
+      case 'I': bluetoothRobot.backLeft(); break;
+      case 'J': bluetoothRobot.backRight(); break;
+      
+      case 'S': bluetoothRobot.stop(); break;
+      case 'Y': bluetoothRobot.horn(); break;
+      case 'U': bluetoothRobot.lightsOn(); break;
+      case 'u': bluetoothRobot.lightsOff(); break;
+      
+      case '1': bluetoothRobot.setSpeed(bluetoothRobot.firstGear); break;
+      case '2': bluetoothRobot.setSpeed(bluetoothRobot.secondGear); break;
+      case '3': bluetoothRobot.setSpeed(bluetoothRobot.thirdGear); break;
+      case '4': bluetoothRobot.setSpeed(bluetoothRobot.fourthGear); break;
+    }
+  }
+}
